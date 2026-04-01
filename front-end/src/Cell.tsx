04b3d23e5427d9b1fcaf7d550c422934033f2b3c
Junhao Li <junhaol2@andrew.cmu.edu>
2026-04-01 16:47:36 -0400

    feat: style change

--- front-end/src/Cell.tsx
+++ front-end/src/Cell.tsx
@@ -8,8 +8,9 @@ interface Props {
 class BoardCell extends React.Component<Props> {
   render(): React.ReactNode {
     const playable = this.props.cell.playable ? 'playable' : '';
+    const markClass = this.props.cell.text === 'X' ? 'mark-x' : this.props.cell.text === 'O' ? 'mark-o' : '';
     return (
-      <div className={`cell ${playable}`}>{this.props.cell.text}</div>
+      <div className={`cell ${playable} ${markClass}`}>{this.props.cell.text}</div>
     )
   }
 }
